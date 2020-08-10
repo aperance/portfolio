251069517877f14edfbc6deb51e66c5fa3c8c8bc
William Aperance <aperance@gmail.com>
2020-08-09 22:11:54 -0700

    Fixed config

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,13 +31,6 @@ module.exports = {
     },
     `gatsby-plugin-smoothscroll`,
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`
-      }
-    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
