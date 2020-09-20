e2f38b23b8bf1cce793d5db6d3486d079aa8571c
William Aperance <aperance@gmail.com>
2020-09-20 03:25:03 -0700

    Moved email to icon, cleanup subtitle

--- src/components/about.js
+++ src/components/about.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { about } from "../../content.yaml";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 import { faLinkedinIn, faGithub } from "@fortawesome/free-brands-svg-icons";
 
 const About = () => {
@@ -11,15 +12,17 @@ const About = () => {
         <span className="text-impact-color">{about.lastName}</span>
       </h1>
       <h4>
-        {about.address}&nbsp;·&nbsp;{about.email}
+        {about.description}&nbsp;&nbsp;·&nbsp;&nbsp;{about.address}
       </h4>
-      <p>{about.description}</p>
       <div>
         <a href={about.linkedin} className="icon" aria-label="linked in">
           <FontAwesomeIcon icon={faLinkedinIn} />
         </a>
         <a href={about.github} className="icon" aria-label="github">
           <FontAwesomeIcon icon={faGithub} />
+        </a>
+        <a href={"mailto:" + about.email} className="icon" aria-label="email">
+          <FontAwesomeIcon icon={faEnvelope} />
         </a>
       </div>
     </div>
